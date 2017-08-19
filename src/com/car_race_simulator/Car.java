94f86b8ffefa828548af182d082d9e1088dcf3ca
Unknown <paalginaa@gmail.com>
2017-08-19 10:18:41 +0200

    Building the core of the program

---
+++ src/com/car_race_simulator/Car.java
@@ -0,0 +1,15 @@
+package com.car_race_simulator;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class Car {
+
+    static void name() {
+        List<String> carNames = new ArrayList<>(Arrays.asList("Patron", "Dragon", "Aura", "Motive", "Crux", "Encounter", "Etheral", "Meridian", "Prestige", "Albatross", "Dynamo", "Lightning", "Utopia", "Prime", "Inferno", "Catalyst", "Sliver", "Stardust", "Nebula", "Vision"));
+        System.out.println(carNames);
+
+    }
+}
--- src/com/car_race_simulator/Main.java
+++ src/com/car_race_simulator/Main.java
@@ -1,6 +1,37 @@
+package com.car_race_simulator;
+
+import java.util.Random;
+
 public class Main {
 
+    static Boolean isRaining() {
+        //30% chance of raining
+        Random random = new Random();
+        int rainChance = random.nextInt();
+        if (rainChance <= 30) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    int createVehicles() {
+        // creates 10 cars, 10 trucks and 10 motorcycles
+        Car car = new Car();
+
+        return 5;
+    }
+
+    void simulateRace() {
+        //calling moveForAnHour() for every vehicle 50 times
+
+    }
+
+    void printRaceResults() {
+        //print out the results of the race
+    }
+
     public static void main(String[] args) {
-        
+        Car.name();
     }
 }
